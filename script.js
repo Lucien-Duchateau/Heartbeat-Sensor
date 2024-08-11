f0daf649b2187a5a128c72cafa6290a6b8f06f10
Lucien-Duchateau <luciendchateau@gmail.com>
2024-08-11 15:53:14 -0700

    c

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let clicks_display = document.getElementById("click_display");
 
 document.body.addEventListener("click", (event) => {
   clicks += 1;
-  clicks_display.innerText(`Clicks: ${clicks}`);
+  clicks_display.innerText = `Clicks: ${clicks}`;
 });
