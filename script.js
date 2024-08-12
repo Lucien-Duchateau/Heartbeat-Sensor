3223128add9b9e7f4513e5a4f4fbf8420d342bb1
Lucien-Duchateau <luciendchateau@gmail.com>
2024-08-11 19:36:53 -0700

    Should work

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let clicks = 0;
-let clicks_display = document.getElementById("click_display");
-
+let playing = false;
 
 document.body.addEventListener("click", (event) => {
-  clicks += 1;
-  clicks_display.innerText = `Clicks: ${clicks}`;
+  if(playing == false) {
+    playing = true;
+    document.body.style = "background-color: green;";
+    calculate_bpm()
+  }
 });
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const calculate_bpm = async () => {
+  await delay(5000);
+  document.body.style = "background-color: red;";
+  let pulses = prompt("Pulses?");
+  let bpm = pulses * 12;
+  alert(`Your BMP is ${bpm}`)
+  playing = false;
+};
